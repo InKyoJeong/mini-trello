0a52dc7db2116461c3be1dd6a90897d3b71c0161
InKyoJeong <viapolar@naver.com>
2021-11-16 17:35:45 +0900

    feat: Get coin name from state - Link의 state로 name을 넘기고 useLocation을 이용하여 다른 페이지에서 가져온다

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -25,9 +25,10 @@ const Coin = styled.li`
   margin-bottom: 10px;
   border-radius: 15px;
   a {
+    display: flex;
+    align-items: center;
     padding: 20px;
     transition: color 0.2s ease-in;
-    display: block;
   }
   &:hover {
     a {
@@ -44,6 +45,12 @@ const Title = styled.h1`
 const Loader = styled.span`
   text-align: center;
   display: block;
+`;
+
+const Img = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-right: 10px;
 `;
 
 interface CoinInterface {
@@ -72,15 +79,25 @@ function Coins() {
   return (
     <Container>
       <Header>
-        <Title>Coins</Title>
+        <Title>Coin Tracker</Title>
       </Header>
       {loading ? (
-        <Loader>로딩중...</Loader>
+        <Loader>Loading...</Loader>
       ) : (
         <CoinsList>
           {coins.map((coin) => (
             <Coin key={coin.id}>
-              <Link to={`/${coin.id}`}>{coin.name} &rarr;</Link>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: { name: coin.name },
+                }}
+              >
+                <Img
+                  src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} &rarr;
+              </Link>
             </Coin>
           ))}
         </CoinsList>
