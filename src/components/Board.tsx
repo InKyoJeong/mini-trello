64c65e628c315afe70f0b2ece8c4a078a54d206e
InKyoJeong <viapolar@naver.com>
2022-01-02 19:25:00 +0900

    chore: delete unnecessary package

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import { Droppable } from "react-beautiful-dnd";
 import { useForm } from "react-hook-form";
 import { ITodo, toDoState } from "../atoms";
--- src/components/DraggableCard.tsx
+++ src/components/DraggableCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Droppable, Draggable } from "react-beautiful-dnd";
+import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
 const Card = styled.div<{ isDragging: boolean }>`
